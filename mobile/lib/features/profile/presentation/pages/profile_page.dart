5260ad35938c0843ec30afe138bb710b1f62edd9
legialuan780 <legialuan780@gmail.com>
2026-06-04 16:30:44 +0700

    fix(mobile): change edit profile ui

--- mobile/lib/features/profile/presentation/pages/profile_page.dart
+++ mobile/lib/features/profile/presentation/pages/profile_page.dart
@@ -47,13 +47,6 @@ class ProfilePage extends StatelessWidget {
                   onTap: () => context.push(RouteNames.viewProfile),
                   isDark: isDark,
                 ),
-                _ProfileMenuItem(
-                  icon: Icons.edit_outlined,
-                  iconColor: const Color(0xFF3B82F6),
-                  title: context.l10n.editProfile,
-                  onTap: () => context.push(RouteNames.editProfile),
-                  isDark: isDark,
-                ),
                 if (isLocalUser)
                   _ProfileMenuItem(
                     icon: Icons.lock_outline_rounded,
